5b76150839bf2e1023a295bf949df07e773899e6
Derek Vogt <derekvogt3@gmail.com>
2022-08-08 15:35:31 -0400

    commit tree 9bfe5fc55811dbd6cbb5757d1ef078a6e5cfcf00

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,13 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        
+        render json: students
+
+    end
+
+    def grades
+        students = Student.all.sort_by { |student| student.grade}.reverse
+        render json: students
+    end 
+end
